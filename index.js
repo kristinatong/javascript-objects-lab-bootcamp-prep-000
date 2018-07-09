be96718ff1cd69f22bfe13a6e065da2fdf0e005d
Kristina Tong <kristina.tong@gmail.com>
2018-07-09 17:10:48 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function updateObjectWithKeyAndValue(object, key, value){
+  var newObj = Object.assign({},object)
+  newObj[key] = value;
+  return newObj
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
+  return object
+}
+
+function deleteFromObjectByKey(object, key){
+  var newObj = Object.assign({},object)
+  delete newObj[key];
+  return newObj
+}
